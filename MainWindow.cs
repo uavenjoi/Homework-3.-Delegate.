67bd7beccca070e6658a7dde3ae75a7eb461b87d
Andrey <metalhead190113@gmail.com>
2017-02-02 22:09:22 +0600

    App done

---
+++ MainWindow.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using Gtk;
+using DelegateGTK;
+
+public partial class MainWindow : Gtk.Window
+{
+	public MainWindow() : base(Gtk.WindowType.Toplevel)
+	{
+		Build();
+	}
+
+	protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+	{
+		Application.Quit();
+		a.RetVal = true;
+	}
+
+	protected void OnStartButtonClicked(object sender, EventArgs e)
+	{
+
+		TextLabel.Text = string.Empty;
+
+		if (ToUpperCheckButton.Active)
+		{
+			if (TextLabel.Text.Length == 0)
+
+				TextLabel.Text = MainClass.StringOperations(TextField.Text, MainClass.ToUpperString);
+			else
+				TextLabel.Text = MainClass.StringOperations(TextLabel.Text, MainClass.ToUpperString);
+		}
+
+		if (TurnStringCheckButton.Active)
+		{
+			if (TextLabel.Text.Length == 0)
+
+				TextLabel.Text = MainClass.StringOperations(TextField.Text, MainClass.TurnString);
+			else
+				TextLabel.Text = MainClass.StringOperations(TextLabel.Text, MainClass.TurnString);
+		}
+
+		if (AddSpacesCheckButton.Active)
+		{
+			if (TextLabel.Text.Length == 0)
+
+				TextLabel.Text = MainClass.StringOperations(TextField.Text, MainClass.AddSpacesToString);
+			else
+				TextLabel.Text = MainClass.StringOperations(TextLabel.Text, MainClass.AddSpacesToString);
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Gtk;
+using System.Collections.Generic;
+
+namespace DelegateGTK
+{
+	class MainClass
+	{
+
+		public delegate string StringWork(string text);
+
+		public static string ToUpperString(string text)
+		{
+			string upperString = text.ToUpper();
+			return upperString;
+		}
+
+		public static string TurnString(string text)
+		{
+			string turnedString = string.Empty;
+
+			for (int i = (text.Length - 1); i >= 0; i -= 1)
+			{
+				turnedString += text[i];
+			}
+
+			return turnedString;
+		}
+
+		public static string AddSpacesToString(string text)
+		{
+			string stringWithSpaces = string.Empty;
+
+			for (int i = 0; i < text.Length; i += 1)
+			{
+				stringWithSpaces += $"{text[i]} ";
+			}
+
+			return stringWithSpaces;
+		}
+
+		public static string StringOperations(string text, StringWork method)
+		{
+			string newString = method(text);
+			return newString;
+		}
+
+		public static void Main(string[] args)
+		{
+			Application.Init();
+			MainWindow win = new MainWindow();
+			win.Show();
+			Application.Run();
+		}
+	}
+}
